349fccdccf46addf76663ec1985ba364cc89910f
goldenbsa <goldenbsa@gmail.com>
2021-05-04 17:28:30 +0300

    добавиль страницу product-details

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,33 @@
 $(function () {
 
+  $('.product-tabs__top-item').on('click', function(e){
+    e.preventDefault();
+    $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
+    $(this).addClass('product-tabs__top-item--active');
+
+    $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
+    $($(this).attr('href')).addClass('product-tabs__content-item--active');
+  });
+
+  $('.product-slide__thumb').slick({
+    asNavFor: '.product-slide__big',
+    focusOnSelect: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    vertical: true,
+    draggable: false,
+  });
+  $('.product-slide__big').slick({
+    asNavFor: '.product-slide__thumb',
+    draggable: false,
+    arrows: false,
+    fade: true
+  });
+
+  $('.select-style, .product-details__num').styler();
+
   $('.filter-price__input').ionRangeSlider({
     type: "double",
-    // step: 0.01,
     onStart: function (data) {
       $('.filter-price__from').text(data.from);
       $('.filter-price__to').text(data.to);
@@ -13,11 +38,19 @@ $(function () {
     },
   });
 
+  $('.slider-related__inner').slick({
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-btn slick-prev" ></button>',
+    infinite: false,
+    slidesToShow: 4,
+    // slidesToScroll: 4,
+  });
+
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
     fade: false,
-    // autoplay: true,
+    autoplay: true,
     autoplaySpeed: 3000
   });
 
